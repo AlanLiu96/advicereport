f85cfc71dc3d73ffc33fd723a47d6aee0a407245
Alan Liu <alanl.liu@yahoo.com>
2015-09-05 03:18:42 -0400

    flask!

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/feed')
+def feed():
+	return render_template('feeds1.txt')
+
+if __name__ == '__main__':
+    app.run()
