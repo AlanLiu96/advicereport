715153268eeb39e0fa870f5940a26c70578635cb
Alan Liu <alanl.liu@yahoo.com>
2015-09-05 13:06:05 -0400

    added a feed of 100 investors in a simplified json

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
-from flask import Flask, Response, request, jsonify
+from flask import Flask, Response, request, jsonify, json
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
-@app.route('/feed')
-def feed():
-	return Response('feeds1.xml',mimetype='text/xml')
 
 if __name__ == '__main__':
     app.run(debug = True)
